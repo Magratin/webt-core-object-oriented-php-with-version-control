6dc7b90a4fe16e4ecee6563a7fbb35f1cf8e544c
Magratin <liupetery@gmail.com>
2024-10-26 15:09:30 +0200

    Formating
    Formated with Prettier

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">    
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Embedded YouTube Videos</title>
     <link rel="stylesheet" href="stylesheet.css">
@@ -9,36 +10,36 @@
 
 <body>
 
-<?php
+    <?php
     require_once 'video.php';
     $youtubevideos = [
-        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY","Bohemian Rhapsody","Youtube"),
-        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY","Bohemian Rhapsody","Youtube"),
-        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY","Bohemian Rhapsody","Youtube"),
-        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY","Bohemian Rhapsody","Youtube"),
-        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY","Bohemian Rhapsody","Youtube")
+        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY", "Bohemian Rhapsody", "Youtube"),
+        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY", "Bohemian Rhapsody", "Youtube"),
+        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY", "Bohemian Rhapsody", "Youtube"),
+        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY", "Bohemian Rhapsody", "Youtube"),
+        new youtubevideo("https://www.youtube.com/embed/tgbNymZ7vqY", "Bohemian Rhapsody", "Youtube")
     ];
 
     $vimeovideos = [
-        new vimeovideo("https://player.vimeo.com/video/76979871","The New Vimeo Player (You Know, For Videos)","Vimeo"),
-        new vimeovideo("https://player.vimeo.com/video/76979871","The New Vimeo Player (You Know, For Videos)","Vimeo"),
-        new vimeovideo("https://player.vimeo.com/video/76979871","The New Vimeo Player (You Know, For Videos)","Vimeo"),
-        new vimeovideo("https://player.vimeo.com/video/76979871","The New Vimeo Player (You Know, For Videos)","Vimeo"),
-        new vimeovideo("https://player.vimeo.com/video/76979871","The New Vimeo Player (You Know, For Videos)","Vimeo")
+        new vimeovideo("https://player.vimeo.com/video/76979871", "The New Vimeo Player (You Know, For Videos)", "Vimeo"),
+        new vimeovideo("https://player.vimeo.com/video/76979871", "The New Vimeo Player (You Know, For Videos)", "Vimeo"),
+        new vimeovideo("https://player.vimeo.com/video/76979871", "The New Vimeo Player (You Know, For Videos)", "Vimeo"),
+        new vimeovideo("https://player.vimeo.com/video/76979871", "The New Vimeo Player (You Know, For Videos)", "Vimeo"),
+        new vimeovideo("https://player.vimeo.com/video/76979871", "The New Vimeo Player (You Know, For Videos)", "Vimeo")
     ];
 
     echo "<p>5 Vimeo Videos</p>";
 
-    foreach($vimeovideos as $vimeovideo) {
+    foreach ($vimeovideos as $vimeovideo) {
         echo $vimeovideo->implementiframe();
     }
 
     echo "<p>5 Youtube Videos</p>";
-    foreach($youtubevideos as $youtubevideo) {
+    foreach ($youtubevideos as $youtubevideo) {
         echo $youtubevideo->implementiframe();
     }
-    
-    
+
+
     ?>
 
     <figure>
@@ -120,6 +121,7 @@
     <figure>
         <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
         <figcaption>Youtube</figcaption>
-    </figure>  
+    </figure>
 </body>
+
 </html>
--- video.php
+++ video.php
@@ -1,44 +1,46 @@
 <?php
-interface videointerface {
+interface videointerface
+{
     function getname();
     function getsource();
     function geturl();
 }
 
-abstract class videoimplement implements videointerface {
-    public function __construct(protected $url, protected $name, protected $source)
+abstract class videoimplement implements videointerface
+{
+    public function __construct(protected $url, protected $name, protected $source) {}
+    function getname()
     {
-    }
-     function getname() {
         return $this->name;
     }
 
-     function getsource() {
+    function getsource()
+    {
         return $this->source;
     }
-     function geturl():string {
+    function geturl(): string
+    {
         return $this->url;
     }
 
-    abstract public function implementiframe():string;
-    
+    abstract public function implementiframe(): string;
 }
 
-class youtubevideo extends videoimplement {
-    function implementiframe():string
-    {
-        return '<iframe width="420" height="315" src="' . $this->url . '" title="' . $this->name . '"></iframe>
-        <figcaption>' . $this->source . '</figcaption>';
-    }
-}
-
-
-class vimeovideo extends videoimplement {
+class youtubevideo extends videoimplement
+{
     function implementiframe(): string
     {
         return '<iframe width="420" height="315" src="' . $this->url . '" title="' . $this->name . '"></iframe>
         <figcaption>' . $this->source . '</figcaption>';
     }
+}
 
+
+class vimeovideo extends videoimplement
+{
+    function implementiframe(): string
+    {
+        return '<iframe width="420" height="315" src="' . $this->url . '" title="' . $this->name . '"></iframe>
+        <figcaption>' . $this->source . '</figcaption>';
     }
-?>
+}
