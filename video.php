99e3d05cb4e5c7bf2c5ee189e0d3868341f28a43
Magratin <liupetery@gmail.com>
2024-10-26 14:55:50 +0200

    Update video.php
    An abstract PHP class for a “video” exists.
    The abstract class implements parts of the interface for a video.
    A Method has been created that returns an iFrame with the Data given to the Properties of the Object

--- video.php
+++ video.php
@@ -4,4 +4,23 @@ interface videointerface {
     function getsource();
     function geturl();
 }
+
+abstract class videoimplement implements videointerface {
+    public function __construct(protected $url, protected $name, protected $source)
+    {
+    }
+     function getname() {
+        return $this->name;
+    }
+
+     function getsource() {
+        return $this->source;
+    }
+     function geturl():string {
+        return $this->url;
+    }
+
+    abstract public function implementiframe():string;
+    
+}
 ?>
