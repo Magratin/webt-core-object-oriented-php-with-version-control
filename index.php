a2c0117976ebec44ec15c4dec28645aeb43a6957
Magratin <liupetery@gmail.com>
2024-10-26 14:49:19 +0200

    UserStory4
    • A plain static HTML view for a single page
    • The view features 20 video entries
    • Each entry features a title, the origin and is embedded
    • The page features CSS
    • The page is semantically valid for HTML5

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embedded YouTube Videos</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>
+    <figure>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Bohemian Rhapsody"></iframe>
+        <figcaption>Youtube</figcaption>
+    </figure>  
+</body>
+</html>
